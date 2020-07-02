8e2441004df61f79cd6b0fbaf84914324bbb8ebe
xuguoxing <xuguoxing@chinadaas.com>
2020-07-02 19:41:56 +0800

    完成一部分业务降级，但是存在问题：每个业务方法都会有一个兜底的方法，会造成代码膨胀。

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/xgx/springcloud/controller/OrderHystrixController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/xgx/springcloud/controller/OrderHystrixController.java
@@ -1,5 +1,7 @@
 package com.xgx.springcloud.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.xgx.springcloud.service.PaymentHystrixService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -25,11 +27,21 @@ public class OrderHystrixController {
         log.info("*******result:"+result);
         return result;
     }
+
+    @HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod",commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "40000")
+    })  //4秒钟以内就是正常的业务逻辑
     @GetMapping("/consumer/payment/hystrix/timeout/{id}")
     public String paymentInfo_TimeOut(@PathVariable("id") Integer id){
         String result = paymentHystrixService.paymentInfo_TimeOut(id);
         log.info("*******result:"+result);
         return result;
     }
+
+    //兜底方法
+    public String paymentTimeOutFallbackMethod(@PathVariable("id") Integer id){
+        return "我是消费者80，对付支付系统繁忙请10秒钟后再试或者自己运行出错请检查自己,(┬＿┬)";
+    }
+
 
 }
