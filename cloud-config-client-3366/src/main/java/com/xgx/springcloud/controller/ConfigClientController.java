e5e4d5cfa1a4207d134571fb844c919211419329
xuguoxing <xuguoxing@chinadaas.com>
2020-07-27 13:01:35 +0800

    增加3366

---
+++ cloud-config-client-3366/src/main/java/com/xgx/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,21 @@
+package com.xgx.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope  //避免远程git配置更改都需要重启客户端的问题
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
+ 
+ 
